fd85df615c2ef060385351b4f362cbc7ad217784
meng160626 <1350257855@qq.com>
2022-08-10 21:26:46 +0800

    全局混入

--- src/main.js
+++ src/main.js
@@ -1,13 +1,32 @@
-import Vue from 'vue'
-import App from './App'
-import uView from 'uni_modules/uview-ui'
+import Vue from "vue";
+import App from "./App";
+import uView from "uni_modules/uview-ui";
+import common from "@/static/js/common.js";
 Vue.use(uView);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-App.mpType = 'app'
+Vue.mixin({
+  data: () => {
+    return {
+      opacity: 0
+    };
+  },
+  methods: {
+    ...common
+  },
+  onPageScroll: function (e) {
+    if (e.scrollTop < 100) {
+      this.opacity = Math.floor(e.scrollTop / 2) / 100;
+    } else {
+      this.opacity = 1;
+    }
+  },
+});
+
+App.mpType = "app";
 
 const app = new Vue({
-  ...App
-})
-app.$mount()
+  ...App,
+});
+app.$mount();
