fae60b6f196fdcb1109c00dfd59f44cedc7b277b
meng160626 <1350257855@qq.com>
2022-08-10 21:27:17 +0800

    卡片头组件

---
+++ src/components/card-title/card-title.vue
@@ -0,0 +1,49 @@
+<template>
+  <view class="card-title fl-between">
+    <view class="left fl-start">
+      <image 
+        v-if="url"
+        :style="{width: width + 'rpx', height: height + 'rpx'}" 
+        :src="'../../static/image/common/' + url" />
+      <text>{{text}}</text>
+    </view>
+    <slot></slot>
+  </view>
+</template>
+
+<script>
+export default {
+  name: "card-title",
+  props: {
+    text: {
+      type: String
+    },
+    url: {
+      type: String
+    },
+    width: {
+      type: Number,
+      default: 32
+    },
+    height: {
+      type: Number,
+      default: 32
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.card-title {
+  padding: 30upx 36upx 24upx 36upx;
+  font-size: 34upx;
+  font-weight: bold;
+  color: #333333;
+  width: 100%;
+  box-sizing: border-box;
+
+  .left {
+    gap: 12upx;
+  }
+}
+</style>
