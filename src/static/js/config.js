b3427c884fb69e53837f918071dae7de6bde9b2e
mzh <Mengzihan0414>
2022-08-17 17:01:08 +0800

    全局配置项

--- src/static/js/config.js
+++ src/static/js/config.js
@@ -1,5 +1,10 @@
 const prevUrl = 'http://localhost:3000/images';
+const requestBaseConfig = {
+    dataType: "json",
+    baseUrl: 'http://localhost:3000'
+};
 
 export default {
-    prevUrl
+    prevUrl,
+    requestBaseConfig
 }
